f226ac3285c45cf393d754e9c0e3d0463a393cf7
Jinsoo <Jinsoo>
2024-01-09 11:45:32 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용, Home 컨텐츠 투명하게 처리
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 
+
 document.addEventListener('scroll', () => {
-    if (window.scrollY > headerHeight) {
-        header.classList.add('header--dark');
-    } else {
-        header.classList.remove('header--dark');
-    }
+  if (window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
+
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
